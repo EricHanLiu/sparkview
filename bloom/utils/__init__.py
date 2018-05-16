d54c7bdf0a6669cf05f76c314f7231f3afcb2d4d
Stefan <stefanmihartescu@gmail.com>
2018-05-16 21:53:18 +0300

    bing alerts, refactoring

--- bloom/utils/__init__.py
+++ bloom/utils/__init__.py
@@ -1,3 +1,4 @@
 from .reporting import (AdwordsReportingService, BingReportingService)
+from .service import (BingService)
 
 __all__ = ["AdwordsReportingService", "BingReportingService"]
--- tasks/bing_tasks.py
+++ tasks/bing_tasks.py
@@ -1,36 +1,13 @@
 from bloom import celery_app
 from bloom import settings
-from bloom.utils import BingReportingService
-from bingads import ServiceClient
 from bing_dashboard import auth
-from bingads.v11.reporting import ReportingServiceManager
+from bloom.utils import BingReportingService
 from bing_dashboard.models import BingAccounts, BingAnomalies
-
-
-def get_services():
-    auth_method = auth.BingAuth().get_auth()
-
-    reporting_manager=ReportingServiceManager(
-          authorization_data=auth_method,
-          poll_interval_in_milliseconds=5000,
-          environment=settings.ENVIRONMENT,
-    )
-
-    reporting_service=ServiceClient(
-        'ReportingService',
-        authorization_data=auth_method,
-        environment=settings.ENVIRONMENT,
-        version=11,
-    )
-
-    return reporting_manager, reporting_service
 
 @celery_app.task(bind=True)
 def bing_cron_anomalies_accounts(self, customer_id):
 
-    services = get_services()
-    helper = BingReportingService(*services)
-
+    helper = BingReportingService()
     current_period_daterange = helper.get_daterange(days=7)
     maxDate = helper.subtract_days(current_period_daterange["minDate"], days=1)
     previous_period_daterange = helper.get_daterange(
@@ -48,36 +25,21 @@ def bing_cron_anomalies_accounts(self, customer_id):
         'Spend'
     ]
 
-    query = helper.get_account_performance_query(
+    report = helper.get_account_performance(
         customer_id,
         dateRangeType="CUSTOM_DATE",
-        aggregation="Daily",
-        report_name="{}_anomalies_curr.csv".format(customer_id),
+        report_name="anomalies_curr",
         extra_fields=fields,
         **current_period_daterange
     )
 
-    query2 = helper.get_account_performance_query(
+    report2 = helper.get_account_performance(
         customer_id,
         dateRangeType="CUSTOM_DATE",
-        aggregation="Daily",
-        report_name="{}_anomalies_prev.csv".format(customer_id),
+        report_name="anomalies_prev",
         extra_fields=fields,
         **previous_period_daterange
     )
-
-    helper.download_report(customer_id, query)
-    helper.download_report(customer_id, query2)
-
-    try:
-        report = helper.get_report(query.ReportName)
-    except FileNotFoundError:
-        report = {}
-
-    try:
-        report2 = helper.get_report(query2.ReportName)
-    except FileNotFoundError:
-        report2 = {}
 
     summed  = helper.sum_report(report)
     summed2  = helper.sum_report(report2)
@@ -108,14 +70,14 @@ def bing_cron_anomalies_accounts(self, customer_id):
         search_impr_share=diff["impressionsharepercent"][0],
         metadata=metadata
     )
+
     #PRINT SUMM DATA
 
 
 @celery_app.task(bind=True)
 def bing_cron_anomalies_campaigns(self, customer_id):
 
-    services = get_services()
-    helper = BingReportingService(*services)
+    helper = BingReportingService()
 
     current_period_daterange = helper.get_daterange(days=7)
     maxDate = helper.subtract_days(current_period_daterange["minDate"], days=1)
@@ -136,44 +98,24 @@ def bing_cron_anomalies_campaigns(self, customer_id):
         'CampaignName'
     ]
 
-    query = helper.get_campaign_performance_query(
+    report = helper.get_campaign_performance(
         customer_id,
         dateRangeType="CUSTOM_DATE",
-        aggregation="Daily",
-        report_name="{}_anomalies_curr.csv".format(customer_id),
+        report_name="anomalies_cmp_curr",
         extra_fields=fields,
         **current_period_daterange
     )
 
-    query2 = helper.get_campaign_performance_query(
+    report2 = helper.get_campaign_performance(
         customer_id,
         dateRangeType="CUSTOM_DATE",
-        aggregation="Daily",
-        report_name="{}_anomalies_prev.csv".format(customer_id),
+        report_name="anomalies_cmp_prev",
         extra_fields=fields,
         **previous_period_daterange
     )
 
-    helper.download_report(customer_id, query)
-    helper.download_report(customer_id, query2)
-
-
-    try:
-        report = helper.get_report(query.ReportName)
-
-    except FileNotFoundError:
-        report = {}
-
-    try:
-        report2 = helper.get_report(query2.ReportName)
-
-    except FileNotFoundError:
-        report2 = {}
-
-
     cmp_stats = helper.map_campaign_stats(report)
     cmp_stats2 = helper.map_campaign_stats(report2)
-    # cmp_stats2 = helper.map_campaign_stats(report2)
     campaign_ids = list(cmp_stats.keys())
     campaign_ids2 = list(cmp_stats2.keys())
 
@@ -218,8 +160,7 @@ def bing_cron_anomalies_campaigns(self, customer_id):
 def bing_cron_ovu(self, customer_id):
 
     account = BingAccounts.objects.get(account_id=customer_id)
-    services = get_services()
-    helper = BingReportingService(*services)
+    helper = BingReportingService()
 
     this_month = helper.get_this_month_daterange()
     last_7 = helper.get_daterange(days=7)
