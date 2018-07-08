4a242d1abfb49e8a71e2c37ffbe363d2a55c846c
Octavian Cristea <octavian.cristea@hotmail.com>
2018-07-08 18:39:25 +0300

    Bug fix for campaign groups

--- tasks/bing_tasks.py
+++ tasks/bing_tasks.py
@@ -301,18 +301,18 @@ def bing_cron_campaign_stats(self, account_id):
     if groupings:
         for gr in groupings:
             for c in cmps:
-                if gr.group_by in c.campaign_name and c in gr.bing_campaigns.all():
-                    gr.current_spend = 0
-                    gr.current_spend += c.campaign_cost
+                if gr.group_by not in c.campaign_name and c in gr.bing_campaigns.all():
+                    gr.bing_campaigns.remove(c)
                     gr.save()
-                elif gr.group_by not in c.campaign_name and c in gr.bing_campaigns.all():
-                    gr.bing_campaigns.delete(c)
-                    gr.current_spend -= c.campaign_cost
-                    gr.save()
+
                 elif gr.group_by in c.campaign_name and c not in gr.bing_campaigns.all():
                     gr.bing_campaigns.add(c)
-                    gr.current_spend += c.campaign_cost
                     gr.save()
+
+            gr.current_spend = 0
+            for cmp in gr.bing_campaigns.all():
+                gr.current_spend += cmp.campaign_cost
+                gr.save()
 
 @celery_app.task(bind=True)
 def bing_cron_flight_dates(self, customer_id):
