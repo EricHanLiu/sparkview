af409a57b96f920eb1143e52be3b74ffe2abaf8c
Sam Creamer <s.creamer@hotmail.com>
2018-11-16 10:34:01 -0500

    remove notification app

--- notifications/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from notifications import views
-
-app_name = "notifications"
-
-urlpatterns = [
-    url(r'^$', views.get_notifications, name="get_notifications")
-]
