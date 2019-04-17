2c1eb458d8b72774cb3242e25e4dfeb852afdcba
Sam Creamer <creamers.mtl@gmail.com>
2019-04-17 14:28:29 -0400

    Set cron ovu back to previous configuriation

--- cron_ovu.py
+++ cron_ovu.py
@@ -4,20 +4,13 @@ import django
 django.setup()
 from adwords_dashboard.models import DependentAccount
 from tasks.adwords_tasks import adwords_cron_ovu
-from user_management.models import Member
 
 
 def main():
     accounts = DependentAccount.objects.filter(blacklisted=False)
-    sam = Member.objects.get(id=1)
-    sams_accounts = []
-    for acc in sam.accounts:
-        for a_acc in acc.adwords.all():
-            sams_accounts.append(a_acc)
     for account in accounts:
         try:
-            if account in sams_accounts:
-                adwords_cron_ovu(account.dependent_account_id)
+            adwords_cron_ovu.delay(account.dependent_account_id)
         except:
             print('exception')
 
