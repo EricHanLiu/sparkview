0c1704fb896d34b706dcb32738e1b21c3aed4c26
Sam Creamer <creamers.mtl@gmail.com>
2019-05-14 14:39:57 -0400

    django honeypot

--- bloom/settings.py
+++ bloom/settings.py
@@ -36,7 +36,9 @@ ALLOWED_HOSTS = [
     "*"
 ]
 ADMINS = [
-    ('Bloom Dev', 'dev@makeitbloom.com')
+    ('Bloom Dev', 'dev@makeitbloom.com'),
+    ('Lexi', 'lexi@makeitbloom.com'),
+    ('Sam', 'sam@makeitbloom.com')
 ]
 # Application definition
 
