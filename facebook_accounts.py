fcbf16e9ebbfc0f05e3d7c6fab360dca723796e7
Sam Creamer <creamers.mtl@gmail.com>
2019-05-17 13:07:02 -0400

    Formatting

--- facebook_accounts.py
+++ facebook_accounts.py
@@ -48,7 +48,8 @@ def add_campaigns():
 
 
 def main():
-    FacebookAdsApi.init(settings.app_id, settings.app_secret, settings.w_access_token, api_version=settings.FACEBOOK_AD_VERSION)
+    FacebookAdsApi.init(settings.app_id, settings.app_secret, settings.w_access_token,
+                        api_version=settings.FACEBOOK_ADS_VERSION)
     me = AdUser(fbid='me')
     accounts = list(me.get_ad_accounts())
     # remove personal AdAccount from list
