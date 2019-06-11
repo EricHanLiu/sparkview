f004de70157b42923f8a7bc8ee10c2c9a223e181
Eric L <ericliu404@gmail.com>
2019-06-11 11:41:48 -0400

    Remove django cron runner

--- cron_prepare_todos.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloom.settings')
-django.setup()
-from notifications.cron import prepare_todos
-
-if __name__ == '__main__':
-    prepare_todos()
