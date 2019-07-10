d858846112c79390554ba46c888936f697555c30
Eric L <ericliu404@gmail.com>
2019-07-10 11:12:46 -0400

    Base template

--- insights/views.py
+++ insights/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from budget.models import Client
 
 
 @login_required
-def insights(request):
+def insights(request, account_id=None):
     if not request.user.is_staff:
         return HttpResponseForbidden('Bye')
 
-    context = {
+    if account_id is None:
+        account = 'Insights'
+    else:
+        account = Client.objects.get(id=account_id)
 
+    context = {
+        'account': account
     }
 
     return render(request, 'insights/insights.html', context)
