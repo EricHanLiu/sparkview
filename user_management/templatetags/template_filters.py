fcfbba0b0ba0368f6a49c75c32916a50c18fb800
Sam Creamer <creamers.mtl@gmail.com>
2019-07-22 10:28:13 -0400

    More rounding problems

--- user_management/templatetags/template_filters.py
+++ user_management/templatetags/template_filters.py
@@ -32,7 +32,7 @@ def get_hours_per_member_and_account(member, account_id):
     """
     Called from template by a member, it fetches how many hours that member has worked on a specific account this month
     """
-    return member.actual_hours_month_by_account(account_id)
+    return round(member.actual_hours_month_by_account(account_id), 2)
 
 
 @register.filter
@@ -40,7 +40,7 @@ def get_allocation_this_month_member(account, member):
     """
     Called from template by an account, it fetches how many allocated hours a member has this month
     """
-    return account.get_allocation_this_month_member(member)
+    return round(account.get_allocation_this_month_member(member), 2)
 
 
 @register.filter
