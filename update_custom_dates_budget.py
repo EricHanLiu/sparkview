48f68c49af1f291de8e2944d1d6a191367957b25
Sam Creamer <creamers.mtl@gmail.com>
2019-08-08 09:42:44 -0400

    Remove bad comments

--- update_custom_dates_budget.py
+++ update_custom_dates_budget.py
@@ -17,16 +17,15 @@ def main():
     except Budget.DoesNotExist:
         print('Error, cannot find this budget')
         return
-    
+
     for aw_camp in budget.aw_campaigns_without_excluded:
         get_spend_by_campaign_custom(aw_camp.id, budget.id)
 
     for fb_camp in budget.fb_campaigns_without_excluded:
         get_spend_by_facebook_campaign_custom(fb_camp.id, budget.id)
 
-    # for bing_camp in budget.bing_campaigns_without_excluded:
-    #     get_spend_by_bing_campaign_custom(bing_camp.id, budget.id)
-
+    for bing_camp in budget.bing_campaigns_without_excluded:
+        get_spend_by_bing_campaign_custom(bing_camp.id, budget.id)
 
 
 main()
