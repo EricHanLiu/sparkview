2dee4cad2c24808a4738393b86b06e8ec4ce94a5
Sam Creamer <creamers.mtl@gmail.com>
2019-08-13 14:32:36 -0400

    Notification crons

--- notifications/cron.py
+++ notifications/cron.py
@@ -1,13 +1,16 @@
+from bloom import celery_app
 from .models import Todo, Notification, ScheduledNotification
 from client_area.models import Promo
 from budget.models import Client
 from adwords_dashboard.models import DependentAccount
 from user_management.models import Member
+from client_area.models import LifecycleEvent
 from django.db.models import Q
 import datetime
 import calendar
 
 
+@celery_app.task(bind=True)
 def prepare_todos():
     """
     Prepare the todolist of each member for the day
@@ -127,3 +130,110 @@ def prepare_todos():
             Todo.objects.create(member=member, description=description, link=link, type=4)
 
         print('Successfully created todos for member %s' % str(member))
+
+    return 'prepare_todos'
+
+
+@celery_app.task(bind=True)
+def create_notifications(self):
+    """
+    Formerly create_notifications.py
+    :param self:
+    :return:
+    """
+    now = datetime.datetime.now()
+    day_of_month = now.day
+    first_weekday, num_in_month = calendar.monthrange(now.year, now.month)
+    negative_day_of_month = num_in_month - now.day + 1  # Gives 1 for last day of month, 2 for second to last day, etc
+    day_of_week = now.weekday()
+
+    if day_of_week < 5:  # weekday
+        scheduled_notifications = ScheduledNotification.objects.filter(Q(days_positive__contains=[day_of_month]) |
+                                                                       Q(days_negative__contains=[
+                                                                           negative_day_of_month]) |
+                                                                       Q(day_of_week=day_of_week) | Q(every_day=True) |
+                                                                       Q(every_week_day=True))
+    else:  # weekend
+        scheduled_notifications = ScheduledNotification.objects.filter(Q(days_positive__contains=[day_of_month]) |
+                                                                       Q(days_negative__contains=[
+                                                                           negative_day_of_month]) |
+                                                                       Q(day_of_week=day_of_week) |
+                                                                       Q(every_day=True))
+
+    for scheduled_notification in scheduled_notifications:
+        """
+        Loop through each notification type and get all of the members that need to be notified
+        """
+        members = Member.objects.none()
+
+        """
+        Get members based on roles
+        """
+        members = members | Member.objects.filter(role__in=scheduled_notification.roles.all())
+
+        """
+        Get members based on teams
+        """
+        members = members | Member.objects.filter(team__in=scheduled_notification.teams.all())
+
+        """
+        Add other members explicitly
+        """
+        members = members | scheduled_notification.members.all()
+
+        for member in members.distinct():
+            """
+            Now go through each member and make the notification
+            """
+            notification = Notification()
+            notification.member = member
+            notification.message = scheduled_notification.message
+            if scheduled_notification.link is not None:
+                notification.link = scheduled_notification.link
+            print('Created notification for ' + str(member.user.get_full_name()))
+            notification.save()
+
+    """
+    Checks for inactive accounts returning from 
+    """
+    one_week_future = datetime.date.today() + datetime.timedelta(7)
+    returning_accounts = Client.objects.filter(inactive_return_date__date=one_week_future)
+
+    for account in returning_accounts:
+        ams = account.assigned_ams
+        for key, val in ams.items():
+            member = val['member']
+            message = account.client_name + ' is inactive but is set to return in one week. Please look into this.'
+            link = '/clients/accounts/' + str(account.id)
+            Notification.objects.create(member=member, message=message, link=link)
+
+    """
+    Alerts account is late
+    """
+    onboarding_accounts = Client.objects.filter(status=0)
+    staff_members = Member.objects.filter(user__is_staff=True)
+
+    for account in onboarding_accounts:
+        if account.onboarding_duration_elapsed == 14:  # TODO: Change this to be a variable
+            ams = account.assigned_ams
+            message = account.client_name + ' is late to onboard. Client services to take action.'
+            link = '/clients/accounts/' + str(account.id)
+            for key, val in ams.items():
+                member = val['member']
+                Notification.objects.get_or_create(member=member, message=message, link=link)
+            for member in staff_members:
+                Notification.objects.get_or_create(member=member, message=message, link=link)
+
+            event_description = account.client_name + ' is late to onboard.'
+            lc_event = LifecycleEvent.objects.create(account=account, type=1, description=event_description,
+                                                     phase=account.phase,
+                                                     phase_day=account.phase_day, cycle=account.ninety_day_cycle,
+                                                     bing_active=account.has_bing,
+                                                     facebook_active=account.has_fb, adwords_active=account.has_adwords,
+                                                     monthly_budget=account.current_budget, spend=account.current_spend)
+
+            if account.late_onboard_reason is not None and account.late_onboard_reason != '':
+                lc_event.notes = 'Reason for late onboard is: ' + account.late_onboard_reason
+
+            lc_event.members.set(account.assigned_members_array)
+            lc_event.save()
