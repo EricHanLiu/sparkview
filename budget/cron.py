defb6ee6e35bc7650758c7026d89045d006fdb25
Sam Creamer <creamers.mtl@gmail.com>
2019-08-13 14:45:05 -0400

    Move a bunch of files to a different folder

--- budget/cron.py
+++ budget/cron.py
@@ -5,6 +5,8 @@ from bing_dashboard.models import BingCampaign
 from calendar import monthrange
 from bloom.utils.utils import perdelta, remove_accents, projected
 from budget.models import Client, AccountBudgetSpendHistory, Budget
+from client_area.models import PhaseTask, PhaseTaskAssignment
+from notifications.models import Notification
 from client_area.models import AccountAllocatedHoursHistory
 from user_management.models import Member, MemberHourHistory
 import datetime
@@ -511,3 +513,78 @@ def daily_context(self):
         record.save()
 
     return 'daily_context'
+
+
+@celery_app.task(bind=True)
+def ninety_days_update(self):
+    """
+    Replaces ninety_days_update.py
+    :param self:
+    :return:
+    """
+    accounts = Client.objects.all()
+    for account in accounts:
+        print(account.client_name + ' day was ' + str(account.phase_day))
+        account.phase_day += 1
+        if account.phase_day == 31:  # Should only last 30 days, therefore on the 31st day we go to new phase
+            account.phase_day = 1
+            if account.phase == 3:
+                account.phase = 1
+            else:
+                account.phase += 1
+        account.save()
+        print(account.client_name + ' day is now ' + str(account.phase_day))
+
+    print('done on ' + str(datetime.datetime.now()))
+
+    for account in accounts:
+        phase = account.phase
+        phase_day = account.phase_day
+        tier = account.tier
+
+        tasks = PhaseTask.objects.filter(phase=phase, day=phase_day, tier=tier)
+        for task in tasks:
+            PhaseTaskAssignment.objects.create(task=task, account=account)
+            print('created task for ' + account.client_name)
+            roles = task.roles
+            members_by_roles = account.members_by_roles(roles)
+            members_to_assign = members_by_roles
+            for member in task.members.all():
+                members_to_assign.append(member)
+            for member in set(members_to_assign):
+                link = '/clients/accounts/' + str(account.id)
+                message = account.client_name + ' task: ' + task.message
+                Notification.objects.create(message=message, link=link, member=member, severity=0, type=0)
+
+    return 'ninety_days_update'
+
+
+@celery_app.task(bind=True)
+def ninety_days_notifications(self):
+    """
+    Ninety days of awesome notifications
+    :param self:
+    :return:
+    """
+    accounts = Client.objects.filter(status=1)
+
+    for account in accounts:
+        phase = account.phase
+        phase_day = account.phase_day
+        tier = account.tier
+
+        tasks = PhaseTask.objects.filter(phase=phase, day=phase_day, tier=tier)
+        for task in tasks:
+            PhaseTaskAssignment.objects.create(task=task, account=account)
+            print('created task for ' + account.client_name)
+            roles = task.roles
+            members_by_roles = account.members_by_roles(roles)
+            members_to_assign = members_by_roles
+            for member in task.members.all():
+                members_to_assign.append(member)
+            for member in set(members_to_assign):
+                link = '/clients/accounts/' + str(account.id)
+                message = account.client_name + ' task: ' + task.message
+                Notification.objects.create(message=message, link=link, member=member, severity=0, type=0)
+
+    return 'ninety_days_notifications'
