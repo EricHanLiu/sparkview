4a7ecd8513adaa7354afd24ed9171349848e759e
Sam Creamer <creamers.mtl@gmail.com>
2019-09-06 13:29:19 -0400

    Hotfix a variety of things

--- scripts/update_custom_dates_budget.py
+++ scripts/update_custom_dates_budget.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 sys.path.append('..')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloom.settings')
 import django
@@ -19,14 +20,20 @@ def main():
         print('Error, cannot find this budget')
         return
 
-    for aw_camp in budget.aw_campaigns_without_excluded:
-        get_spend_by_campaign_custom(aw_camp.id, budget.id)
+    if budget.is_monthly:
+        return
 
-    for fb_camp in budget.fb_campaigns_without_excluded:
-        get_spend_by_facebook_campaign_custom(fb_camp.id, budget.id)
+    if budget.has_adwords:
+        for aw_account in budget.account.adwords.all():
+            get_spend_by_campaign_custom(budget.id, aw_account.id)
 
-    for bing_account in budget.account.bing.all():
-        get_spend_by_bing_campaign_custom(budget.id, bing_account.id)
+    if budget.has_facebook:
+        for fb_account in budget.account.facebook.all():
+            get_spend_by_facebook_campaign_custom(budget.id, fb_account.id)
+
+    if budget.has_bing:
+        for bing_account in budget.account.bing.all():
+            get_spend_by_bing_campaign_custom(budget.id, bing_account.id)
 
 
 main()
