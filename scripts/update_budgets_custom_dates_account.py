201e8140fb2764dc8e18a98f4c1d2b396054aef3
Sam Creamer <creamers.mtl@gmail.com>
2019-09-06 10:09:46 -0400

    Refactor facebook

--- scripts/update_budgets_custom_dates_account.py
+++ scripts/update_budgets_custom_dates_account.py
@@ -29,13 +29,13 @@ def main():
         #     for aw_account in budget.account.adwords.all():
         #         get_spend_by_campaign_custom(budget.id, aw_account.id)
         #
-        # if budget.has_facebook:
-        #     for fb_camp in budget.fb_campaigns_without_excluded:
-        #         get_spend_by_facebook_campaign_custom(fb_camp.id, budget.id)
+        if budget.has_facebook:
+            for fb_account in budget.account.facebook.all():
+                get_spend_by_facebook_campaign_custom(budget.id, fb_account.id)
 
-        if budget.has_bing:
-            for bing_account in budget.account.bing.all():
-                get_spend_by_bing_campaign_custom(budget.id, bing_account.id)
+        # if budget.has_bing:
+        #    for bing_account in budget.account.bing.all():
+        #        get_spend_by_bing_campaign_custom(budget.id, bing_account.id)
 
 
 main()
