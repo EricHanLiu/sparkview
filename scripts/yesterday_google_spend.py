f8c2d7bd450d2a33901fe4ae6ce7365a40278779
Sam Creamer <creamers.mtl@gmail.com>
2019-09-26 08:50:38 -0400

    Fix oder of operations

--- scripts/yesterday_google_spend.py
+++ scripts/yesterday_google_spend.py
@@ -4,10 +4,11 @@ import sys
 sys.path.append('..')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloom.settings')
 import django
+django.setup()
+
 from bloom.utils.ppc_accounts import active_adwords_accounts
 from tasks.adwords_tasks import adwords_cron_campaign_stats
 
-django.setup()
 accounts = active_adwords_accounts()
 for account in accounts:
     adwords_cron_campaign_stats.delay(account.dependent_account_id)
