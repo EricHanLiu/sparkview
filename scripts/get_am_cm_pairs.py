6c93b8791c251303a978f00c3108cbb0e45a74c0
Sam Creamer <creamers.mtl@gmail.com>
2019-10-11 13:48:15 -0400

    AM, CM pairs

---
+++ scripts/get_am_cm_pairs.py
@@ -0,0 +1,41 @@
+# This script gets pairs of AM1s and CM1s and lists the accounts they are on together
+# Writte by Sam Creamer
+# October 2019
+import os
+import sys
+
+sys.path.append('..')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloom.settings')
+import django
+
+django.setup()
+from budget.models import Client
+
+
+def main():
+    accounts = Client.objects.filter(status=1)
+
+    am_cm_pairs = {}
+
+    for account in accounts:
+        if account.am1 is None or account.cm1 is None:
+            continue
+
+        cm = account.cm1
+        am = account.am1
+
+        key = str(cm) + ', ' + str(am)
+
+        if key not in am_cm_pairs:
+            am_cm_pairs[key] = []
+
+        am_cm_pairs[key].append(str(account))
+
+    for key in am_cm_pairs:
+        print('========================')
+        print(key)
+        print(am_cm_pairs[key])
+        print('========================')
+
+
+main()
